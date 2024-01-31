f08785f7c4d4a3ff734bcfd1e6152dc56b77de05
Nico Burger <nburger@theaiinstitute.com>
2024-01-31 21:43:31 +0000

    .vscode set up, including recent extension changes

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ISAAC_PATH=${ORBIT_PATH}/_isaac_sim
 export CARB_APP_PATH=${ISAAC_PATH}/kit
-export EXP_PATH=${ISAAC_PATH}/apps
+export EXP_PATH=${ISAAC_PATH}/appss
 source ${ISAAC_PATH}/setup_python_env.sh
-printenv >${workspaceFolder}/.vscode/.python.env
-${ISAAC_PATH}/python.sh ${workspaceFolder}/.vscode/tools/setup_vscode.py
+# printenv >${workspaceFolder}/.vscode/.python.env
+${ISAAC_PATH}/python.sh ${workspaceFolder}.vscode/tools/setup_vscode.py ${ISAAC_PATH}
